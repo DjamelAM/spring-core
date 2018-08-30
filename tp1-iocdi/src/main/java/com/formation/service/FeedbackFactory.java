5352cb8361aa08f7bfffad64caddf8e9bfc9d082
unknown <amraoui.djamel@gmail.com>
2018-08-30 12:05:15 +0200

    tp1

---
+++ tp1-iocdi/src/main/java/com/formation/service/FeedbackFactory.java
@@ -0,0 +1,12 @@
+package com.formation.service;
+
+public class FeedbackFactory {
+	/**
+	 * By default, a {@link FeedbackFactory} return an instance of
+	 * {@link FeedbackToConsole}
+	 */
+	public IFeedback getFeedback() {
+		return new FeedbackToConsole();
+	}
+
+}
---
+++ tp1-iocdi/src/main/java/com/formation/service/IFeedback.java
@@ -0,0 +1,9 @@
+package com.formation.service;
+
+/**
+ * Interface to give feedback to the user
+ *
+ */
+public interface IFeedback {
+	public void say(String something);
+}
