ff6c8048707d2b4436382128672b3c00f0b6f992
unknown <amraoui.djamel@gmail.com>
2018-08-30 14:24:56 +0200

    tp2

--- tp1-iocdi/src/main/java/com/formation/App.java
+++ tp1-iocdi/src/main/java/com/formation/App.java
@@ -1,13 +1,18 @@
 package com.formation;
 
+import com.formation.service.FeedbackFactory;
+import com.formation.user.UserInteraction;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		UserInteraction userInteraction = new UserInteraction();
+		userInteraction.setFactory(new FeedbackFactory());
+		userInteraction.sayHello("Dylan");
+		userInteraction.sayGoodBye("Dylan");
+	}
 }
---
+++ tp1-iocdi/src/main/java/com/formation/service/FeedbackToConsole.java
@@ -0,0 +1,10 @@
+package com.formation.service;
+
+/**
+ * Implementation of {@link IFeedback} using the console as output
+ */
+public class FeedbackToConsole implements IFeedback {
+	public void say(String something) {
+		System.out.println(something);
+	}
+}
